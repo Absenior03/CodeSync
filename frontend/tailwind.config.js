8416f30c3f7b8d9ca7d5052ba6a9171ef4a88d6e
Anirban Banerjee <anirbanbanerjee050@gmail.com>
2025-08-07 12:27:08 +0530

    font update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,19 @@
+// frontend/tailwind.config.js
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // Set 'Inter' as the default font family
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
